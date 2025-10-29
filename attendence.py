0c93d1f07666a9aeb1daeb2085e70410058fb056
awalikar886 <awalikar886@gmail.com>
2025-10-29 20:06:14 +0530

    Create attendence.py

---
+++ attendence.py
@@ -0,0 +1,6 @@
+classes_held = int(input("Enter total number of classes held: "))
+classes_attended = int(input("Enter total number of classes attended: "))
+attendance = (classes_attended / classes_held) * 100
+print(f"Classes Held: {classes_held}")
+print(f"Classes Attended: {classes_attended}")
+print(f"Attendance: {attendance:.0f}%")
